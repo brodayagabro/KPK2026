693423e531c0b669f930e30c9732536288183c75
brodayagabro <brodyagabro@bk.ru>
2026-03-17 15:04:57 +0300

    Add pot poject on Adruino

---
+++ project/sketch.ino
@@ -0,0 +1,21 @@
+#include "Servo.h"
+
+int potPin = A0;
+int potVal = 0;
+int threshold = 512;
+servo serv1;
+void setup() {
+  serv1.attach(5);
+  Serial.begin(9600);
+}
+
+void loop() {
+  potVal = analogRead(potPin);
+  if (potVal >= threshold){
+    serv1.write(90);
+  } else {
+    serv1.write(0);
+  }
+  Serial.println(potVal);
+  delay(10);
+}
